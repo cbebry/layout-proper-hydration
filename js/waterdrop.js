27c97fcf15602f24d445bc901846753065606719
Christopher Bebry <invalidsyntax@gmail.com>
2013-06-22 02:52:47 -0400

    Dynamic waterdrop sizing algo done

--- js/waterdrop.js
+++ js/waterdrop.js
@@ -9,23 +9,34 @@ var waterdrop_init = function(canvas, context) {
 	}
 }
 var waterdrop_update = function(canvas, context) {
-	var show_percent = 50;
+	//must be between 0 and 100
+	var show_percent = 50; //TODO: based on input
 	
 	var w = canvas[0].width;
 	var h = canvas[0].height;
 	
+	// var sw = w;
+	// var sh = h - (show_percent * h / 100);
+	// var sx = h - sh;
+	// var sy = 0;
+	
+	// var dw = sw;
+	// var dh = sh;
+	// var dx = 0;
+	// var dy = 0;
+	
 	var sw = w;
-	var sh = h - (show_percent * h / 100);
-	var sx = h - sh;
-	var sy = 0;
+	var sh = h - ((100-show_percent) * h / 100);
+	var sx = 0;
+	var sy = h - sh;
 	
 	var dw = sw;
 	var dh = sh;
-	var dx = 0;
-	var dy = 0;
+	var dx = sx;
+	var dy = sy;
 	
 	
-	context.drawImage(waterdrop_outline, 0, 0, w, h);
+	//context.drawImage(waterdrop_outline, 0, 0, w, h);
 	context.drawImage(waterdrop_fill, sx, sy, sw, sh, dx, dy, dw, dh);
 	//console.log(canvas[0].width);
 	//console.log(canvas[0].height);
