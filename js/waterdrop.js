77dccb4c920cf241db647bcf44cbe928c36047a6
Christopher Bebry <invalidsyntax@gmail.com>
2013-06-22 13:32:56 -0400

    Two input values working

--- js/waterdrop.js
+++ js/waterdrop.js
@@ -3,10 +3,24 @@ waterdrop_outline.src = 'images/waterdrop.png';
 var waterdrop_fill = new Image();
 waterdrop_fill.src = 'images/waterdrop-fill.png';
 
+var updatePercent = function(glasses) {
+	//TODO
+}
+
 var waterdrop_init = function(canvas, context) {
 	waterdrop_outline.onload = function() {
 		waterdrop_update(canvas, context);
 	}
+	$('#volume').blur(function(e) {
+		var volume = $('#volume').val();
+		var glasses = volume / 8;
+		$('#glasses').val(glasses);
+	});
+	$('#glasses').blur(function(e) {
+		var glasses = $('#glasses').val();
+		var volume = glasses * 8;
+		$('#volume').val(volume);
+	});
 }
 var waterdrop_update = function(canvas, context) {
 	//must be between 0 and 100
@@ -31,6 +45,4 @@ var waterdrop_update = function(canvas, context) {
 	
 	context.drawImage(waterdrop_outline, 0, 0, w, h);
 	context.drawImage(waterdrop_fill, sx, sy, sw, sh, dx, dy, dw, dh);
-	//console.log(canvas[0].width);
-	//console.log(canvas[0].height);
 }
